98984625ce568140e71277450f0623c5c32e1f34
M4rcoToni <marcosanto.sl021@academico.ifsul.edu.br>
2023-03-31 09:48:21 -0300

    add flatlist

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -2,17 +2,39 @@ import { GroupCard } from '@components/GroupCard';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { Container } from './styles';
+import { FlatList } from 'react-native';
+import { ListEmpty } from '@components/ListEmpty';
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>([]);
+
   return (
     <Container >
+
       <Header />
+
       <Highlight
         title='Turmas'
         subtitle='Jogue com sua turma!'
       />
-      <GroupCard title='Galera do Ignite' onPress={() => { }} />
+
+      <FlatList
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <GroupCard
+            title={item}
+            onPress={() => { }}
+          />
+        )}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty message='Que tal cadastrar a primeira turma?' />
+        )}
+      />
+
     </Container>
   );
 }
